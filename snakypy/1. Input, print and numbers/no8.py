37bad6d91fe75c74ba64af655460b1c8bab77104
Shahriar Mamun <71685588+shahriar859@users.noreply.github.com>
2021-01-22 17:01:04 +0600

    Create no8.py

---
+++ snakypy/1. Input, print and numbers/no8.py
@@ -0,0 +1,11 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+
+a = ((h1*60*60)+(m1*60)+s1)
+b = ((h2*60*60)+(m2*60)+s2)
+
+print(b-a)
